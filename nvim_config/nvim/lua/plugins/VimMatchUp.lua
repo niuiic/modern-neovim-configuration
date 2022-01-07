8275d02d6a8b60d5078abec6583fb1791bedbbff
niuiic <niuiicde@gmail.com>
2022-01-07 21:43:15 +0800

    clean

--- nvim_config/nvim/lua/plugins/VimMatchUp.lua
+++
@@ -1,8 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-    matchup = {
-        enable = true, -- mandatory, false will disable the whole extension
-        disable = {}, -- optional, list of language that will be disabled
-        disable_virtual_text = true,
-        include_match_words = {}
-    }
-}
