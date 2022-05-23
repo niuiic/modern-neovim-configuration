88d02fbd8b11231c9d19efedc2c13d3d428de770
niuiic <niuiicde@gmail.com>
2022-05-23 22:27:41 +0800

    update

--- lua/lsp/config/bash.lua
+++
@@ -1,3 +0,0 @@
--- local runtime_path = vim.split(package.path, ";")
--- table.insert(runtime_path, "bash/?.lua")
--- table.insert(runtime_path, "bash/?/init.lua")
--- lua/plugin/vim-floaterm.lua
+++
@@ -1,4 +0,0 @@
-vim.g.floaterm_width = 1
-vim.g.floaterm_height = 0.3
-vim.g.floaterm_wintype = "split"
-vim.g.floaterm_rootmarkers = ".root"
