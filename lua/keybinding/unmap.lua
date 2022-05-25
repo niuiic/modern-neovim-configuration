f5de6b310d04f640f2cf67602196e334a8487d0d
niuiic <niuiicde@gmail.com>
2022-05-25 11:27:51 +0800

    update

---
+++ lua/keybinding/unmap.lua
@@ -0,0 +1,6 @@
+local utils = require("utils")
+
+-- vim-gitgutter
+utils.fn.map("n", "<leader>hp", "", utils.var.opt)
+utils.fn.map("n", "<leader>hs", "", utils.var.opt)
+utils.fn.map("n", "<leader>hu", "", utils.var.opt)
