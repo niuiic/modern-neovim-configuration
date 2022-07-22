deee6a65c9281040fd223cf7bdec06090d6861a0
niuiic <niuiicde@gmail.com>
2022-07-22 16:39:18 +0800

    update snippets

--- lua/snippet/javascript.lua
+++ lua/snippet/javascript.lua
@@ -9,18 +9,12 @@ local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
-local l = require("luasnip.extras").lambda
-local rep = require("luasnip.extras").rep
-local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
-local dl = require("luasnip.extras").dynamic_lambda
-local events = require("luasnip.util.events")
-local ai = require("luasnip.nodes.absolute_indexer")
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.expand_conditions")
+local events = utils.fn.require("luasnip.util.events")
+local ai = utils.fn.require("luasnip.nodes.absolute_indexer")
+local fmt = utils.fn.require("luasnip.extras.fmt").fmt
+local m = utils.fn.require("luasnip.extras").m
+local lambda = utils.fn.require("luasnip.extras").l
+local postfix = utils.fn.require("luasnip.extras.postfix").postfix
 
 local M = {
 	s("print", {
--- lua/source/framework/element-plus.lua
+++ lua/source/framework/element-plus.lua
@@ -1,5 +1,4 @@
 local M = {
-	{ label = "gutter" },
 	{ label = "justify" },
 }
 
