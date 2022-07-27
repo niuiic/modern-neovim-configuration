1b8e3db17c963ce5986f60a49fef660f9bfde573
niuiic <niuiicde@gmail.com>
2022-07-27 12:02:21 +0800

    disable rename feature of cssls in vue files

--- lua/lsp/cssls.lua
+++ lua/lsp/cssls.lua
@@ -1,19 +1,17 @@
+local utils = require("utils")
+
 local M = {
 	modify_capabilities = function(capabilities)
 		capabilities.textDocument.completion.completionItem.snippetSupport = true
-		-- disable rename feature
-		capabilities.rename = {
-			dynamicRegistration = false,
-			prepareSupport = false,
-		}
-		capabilities.textDocument.rename = {
-			dynamicRegistration = false,
-			prepareSupport = false,
-		}
-		-- FIXME: unable to disable rename feature
 		return capabilities
 	end,
+	on_attach = function(client, _)
+		if vim.bo.filetype == "vue" then
+			client.server_capabilities.renameProvider = false -- disable rename feature
+		end
+	end,
 	filetypes = { "css", "scss", "less", "vue" },
+	root_dir = utils.fn.root_pattern,
 }
 
 return M
