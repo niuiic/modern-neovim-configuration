03180f647575a57109d25f195cddfea2f7aae430
niuiic <niuiicde@gmail.com>
2022-08-15 08:19:24 +0800

    fix comment style

--- lua/plugin/comment.lua
+++ lua/plugin/comment.lua
@@ -30,32 +30,23 @@ utils.fn.require("Comment").setup({
 		extended = false,
 	},
 	pre_hook = function(ctx)
-		if vim.bo.filetype == "vue" then
-			local U = utils.fn.require("Comment.utils")
+		-- Only calculate commentstring for tsx filetypes
+		local U = require("Comment.utils")
 
-			-- Determine whether to use linewise or blockwise commentstring
-			local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
+		-- Determine whether to use linewise or blockwise commentstring
+		local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
 
-			-- Determine the location where to calculate commentstring from
-			local location = nil
-			if ctx.ctype == U.ctype.blockwise then
-				location = utils.fn.require("ts_context_commentstring.utils").get_cursor_location()
-			elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-				location = utils.fn.require("ts_context_commentstring.utils").get_visual_start_location()
-			end
-
-			return utils.fn.require("")("ts_context_commentstring.internal").calculate_commentstring({
-				key = type,
-				location = location,
-			})
+		-- Determine the location where to calculate commentstring from
+		local location = nil
+		if ctx.ctype == U.ctype.blockwise then
+			location = require("ts_context_commentstring.utils").get_cursor_location()
+		elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+			location = require("ts_context_commentstring.utils").get_visual_start_location()
 		end
+
+		return require("ts_context_commentstring.internal").calculate_commentstring({
+			key = type,
+			location = location,
+		})
 	end,
 })
-
-local ft = utils.fn.require("Comment.ft")
-ft({ "go", "rust", "javascript", "typescript" }, "//%s")
-ft({ "lua" }, "--%s")
-
--- keymap
-utils.fn.map("v", "<C-a>", "<Plug>(comment_toggle_linewise_visual)", utils.var.opt)
-utils.fn.map("n", "<C-a>", "<Plug>(comment_toggle_linewise_current)", utils.var.opt)
