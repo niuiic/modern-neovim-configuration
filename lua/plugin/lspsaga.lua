83a8f50432eab2ce42066d7b1961321b94ce1f0c
niuiic <niuiicde@gmail.com>
2022-09-17 09:56:17 +0800

    update lsp icons

--- lua/plugin/lspsaga.lua
+++ lua/plugin/lspsaga.lua
@@ -179,3 +179,9 @@ utils.fn.whichKeyMap({
 		},
 	},
 }, { mode = "v" })
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
