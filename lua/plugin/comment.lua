ec593aaecfc096e6ca810939d263c77c363c3aa4
niuiic <niuiicde@gmail.com>
2022-10-04 22:05:53 +0800

    fix commentstring for vue file

--- lua/plugin/comment.lua
+++ lua/plugin/comment.lua
@@ -1,21 +1,37 @@
 local utils = require("utils")
 
--- FIXME: not worked in vue files
 utils.fn.require("nvim-treesitter.configs").setup({
 	context_commentstring = {
 		enable = true,
 		enable_autocmd = false,
-		config = {
-			vue = {
-				__default = "<!-- %s -->",
-				html = "<!-- %s -->",
-				style = "/* %s */",
-				script = "// %s",
-				comment = "<!-- %s -->",
-			},
-		},
 	},
 })
+
+local calc_vue_commentstring = function()
+	local default_commentstring = "<!-- %s -->"
+	local ts_utils = utils.fn.require("nvim-treesitter.ts_utils")
+
+	local current_node = ts_utils.get_node_at_cursor()
+	if not current_node then
+		return default_commentstring
+	end
+
+	local node = current_node
+	while node do
+		if node:type() == "stylesheet" then
+			return "/* %s */"
+		elseif node:type() == "program" then
+			return "// %s"
+		elseif node:type() == "component" then
+			return "<!-- %s -->"
+		end
+		if node:parent() then
+			node = node:parent()
+		else
+			return default_commentstring
+		end
+	end
+end
 
 utils.fn.require("Comment").setup({
 	mappings = {
@@ -24,7 +40,13 @@ utils.fn.require("Comment").setup({
 		extended = false,
 	},
 	ignore = "^$",
-	pre_hook = utils.fn.require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+	pre_hook = function(ctx)
+		if vim.bo.filetype == "vue" then
+			return calc_vue_commentstring()
+		else
+			return utils.fn.require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(ctx)
+		end
+	end,
 })
 
 utils.fn.map("v", "<C-a>", "<Plug>(comment_toggle_linewise_visual)", utils.var.opt)
