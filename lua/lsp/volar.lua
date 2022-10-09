8e650070a15683ee078a4bfb2fdf7956864061d7
niuiic <niuiicde@gmail.com>
2022-10-09 08:36:42 +0800

    update volar config

--- lua/lsp/volar.lua
+++ lua/lsp/volar.lua
@@ -9,38 +9,6 @@ local M = {
 	filetypes = filetypes,
 	root_dir = utils.fn.root_pattern,
 	init_options = {
-		documentFeatures = {
-			documentColor = false,
-			documentFormatting = {
-				defaultPrintWidth = 100,
-			},
-			documentSymbol = true,
-			foldingRange = true,
-			linkedEditingRange = true,
-			selectionRange = true,
-		},
-		languageFeatures = {
-			callHierarchy = true,
-			codeAction = true,
-			codeLens = true,
-			completion = {
-				defaultAttrNameCase = "kebabCase",
-				defaultTagNameCase = "both",
-			},
-			definition = true,
-			diagnostics = true,
-			documentHighlight = true,
-			documentLink = true,
-			hover = true,
-			implementation = true,
-			references = true,
-			rename = true,
-			renameFileRefactoring = true,
-			schemaRequestService = true,
-			semanticTokens = false,
-			signatureHelp = true,
-			typeDefinition = true,
-		},
 		typescript = {
 			serverPath = os.getenv("HOME")
 				.. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib/tsserverlibrary.js",
