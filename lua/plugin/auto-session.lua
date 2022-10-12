6190001d0730fe55b0926595e587c39980e5d765
niuiic <niuiicde@gmail.com>
2022-10-12 10:20:38 +0800

    update config for auto-session

--- lua/plugin/auto-session.lua
+++ lua/plugin/auto-session.lua
@@ -1,15 +1,8 @@
 local utils = require("utils")
 
 utils.fn.require("auto-session").setup({
-	log_level = "error",
-	auto_session_enable_last_session = false,
-	auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
 	auto_session_enabled = false,
-	auto_save_enabled = false,
-	auto_restore_enabled = false,
-	auto_session_suppress_dirs = nil,
-	auto_session_allowed_dirs = "*",
-	auto_session_use_git_branch = nil,
+	auto_session_create_enabled = false,
 })
 
 -- keymap
