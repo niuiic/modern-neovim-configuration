e66815ade69261b1cc38b3bafacefdb5bb6d674a
niuiic <niuiicde@gmail.com>
2022-11-26 11:13:23 +0800

    update

--- lua/plugin/lspkind-nvim.lua
+++ lua/plugin/lspkind-nvim.lua
@@ -1,21 +1,8 @@
 local utils = require("utils")
 
 utils.fn.require("lspkind").init({
-	-- default: true
-	-- with_text = true,
-	-- defines how annotations are shown
-	-- default: symbol
-	-- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
 	mode = "symbol_text",
-	-- default symbol map
-	-- can be either 'default' (requires nerd-fonts font) or
-	-- 'codicons' for codicon preset (requires vscode-codicons font)
-	--
-	-- default: 'default'
 	preset = "codicons",
-	-- override preset symbols
-	--
-	-- default: {}
 	symbol_map = {
 		Text = "",
 		Method = "",
