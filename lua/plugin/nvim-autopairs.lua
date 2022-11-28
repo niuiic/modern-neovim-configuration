781468ffe915f01f5ab4c598c0fb0d957c438362
niuiic <niuiicde@gmail.com>
2022-11-28 20:56:31 +0800

    correct autopair setting

--- lua/plugin/nvim-autopairs.lua
+++ lua/plugin/nvim-autopairs.lua
@@ -3,5 +3,6 @@ local utils = require("utils")
 local autopairs = utils.fn.require("nvim-autopairs")
 local rule = utils.fn.require("nvim-autopairs.rule")
 
-autopairs.add_rules(rule("'", "'", "-rust"))
 autopairs.setup({})
+autopairs.remove_rule("'")
+autopairs.add_rule(rule("'", "'", "-rust"))
