07e2418ef1d1402bfad684e3fb3e2bd9d10f67e3
niuiic <niuiicde@gmail.com>
2022-11-28 20:34:11 +0800

    disable autopair of single quote for rust

--- lua/plugin/nvim-autopairs.lua
+++ lua/plugin/nvim-autopairs.lua
@@ -1,3 +1,7 @@
 local utils = require("utils")
 
-utils.fn.require("nvim-autopairs").setup()
+local autopairs = utils.fn.require("nvim-autopairs")
+local rule = utils.fn.require("nvim-autopairs.rule")
+
+autopairs.add_rules(rule("'", "'", "-rust"))
+autopairs.setup({})
