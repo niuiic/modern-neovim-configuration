cb99313718fb9ce2fb3f60f5da84aa9406de5ea5
niuiic <niuiicde@gmail.com>
2022-12-14 14:20:39 +0800

    prefer to use local typescript version

--- lua/lsp/volar.lua
+++ lua/lsp/volar.lua
@@ -5,13 +5,21 @@ if utils.fn.match_str_in_file(utils.fn.root_pattern() .. "/package.json", "vue")
 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" }
 end
 
+local function search_ts_server_path()
+	local local_ts_server_path = utils.fn.root_pattern() .. "/node_modules/typescript/lib"
+	if utils.fn.file_exists(local_ts_server_path .. "/lib.d.ts") then
+		return local_ts_server_path
+	else
+		return os.getenv("HOME") .. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib"
+	end
+end
+
 local M = {
 	filetypes = filetypes,
 	root_dir = utils.fn.root_pattern,
 	init_options = {
 		typescript = {
-			tsdk = os.getenv("HOME")
-				.. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib",
+			tsdk = search_ts_server_path(),
 		},
 	},
 }
