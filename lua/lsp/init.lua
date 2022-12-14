a3f557d150ec0f8ebd3e59778950a886ed45c5a0
niuiic <niuiicde@gmail.com>
2022-12-14 20:01:11 +0800

    update lsp config

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -158,12 +158,6 @@ utils.fn.whichKeyMap({
 	prefix = "<localleader>",
 })
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = "single",
-})
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	border = "single",
-})
 vim.diagnostic.config({
 	float = { border = "single" },
 })
