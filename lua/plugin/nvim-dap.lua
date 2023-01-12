a4663b5061eb07f8683b5df56e5876c92bbd6a53
niuiic <niuiicde@gmail.com>
2023-01-12 11:47:56 +0800

    update

--- lua/plugin/nvim-dap.lua
+++ lua/plugin/nvim-dap.lua
@@ -7,12 +7,6 @@ local config = function()
 	dap.listeners.after.event_initialized.dapui_config = function()
 		dapui.open({})
 	end
-	dap.listeners.before.event_terminated.dapui_config = function()
-		dapui.close({})
-	end
-	dap.listeners.before.event_exited.dapui_config = function()
-		dapui.close({})
-	end
 
 	require("nvim-dap-virtual-text").setup({})
 
@@ -138,6 +132,7 @@ local keys = {
 		function()
 			require("dap").terminate()
 			require("dapui").close({})
+			require("dap.repl").close({})
 		end,
 		desc = "quit debug",
 	},
