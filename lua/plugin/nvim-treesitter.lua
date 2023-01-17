3666eedd8efe1cff096f5e850c91c66a57f315db
niuiic <niuiicde@gmail.com>
2023-01-17 09:57:08 +0800

    update

--- lua/plugin/nvim-treesitter.lua
+++ lua/plugin/nvim-treesitter.lua
@@ -30,6 +30,7 @@ local config = function()
 			"yaml",
 			"glsl",
 			"wgsl",
+			"mermaid",
 		},
 		highlight = { enable = true, additional_vim_regex_highlighting = false },
 		intent = { enable = true },
