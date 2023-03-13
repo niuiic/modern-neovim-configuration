9df0ecc4bde5a55629215ea54fcf3ce4d8b37f0c
niuiic <niuiicde@gmail.com>
2023-03-13 19:00:50 +0800

    update

--- lua/plugin/nvim-tree.lua
+++ lua/plugin/nvim-tree.lua
@@ -126,7 +126,7 @@ end
 return {
 	config = config,
 	keys = {
-		{ "<space>oF", "<cmd>NvimTreeFindFile<CR>", desc = "open file tree" },
+		{ "<space>oF", "<cmd>NvimTreeFocus<CR>", desc = "open file tree" },
 	},
 	dependencies = {
 		"kyazdani42/nvim-web-devicons",
