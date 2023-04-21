2b24ac71781a0ffb6973680057b0b7554625c4ee
niuiic <niuiicde@gmail.com>
2023-04-21 08:47:10 +0800

    update

--- lua/plugin/bufferline.lua
+++ lua/plugin/bufferline.lua
@@ -13,7 +13,6 @@ local config = function()
 			color_icons = true,
 			show_buffer_icons = true,
 			show_buffer_close_icons = false,
-			show_buffer_default_icon = true,
 			show_close_icon = false,
 			show_tab_indicators = false,
 			persist_buffer_sort = true,
