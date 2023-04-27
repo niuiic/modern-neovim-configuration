ef8cea9141065c64e625ade487fdcecb1bfa89e1
niuiic <niuiicde@gmail.com>
2023-04-27 11:46:23 +0800

    update

--- lua/plugin/multiple-session.lua
+++ lua/plugin/multiple-session.lua
@@ -1,9 +1,14 @@
+local get_buf_name = function(bufnr, root_path)
+	return string.sub(vim.api.nvim_buf_get_name(bufnr), string.len(root_path) + 2)
+end
+
 local store_breakpoints = function(file_path)
 	local core = require("niuiic-core")
+	local root_path = core.file.root_path()
 
 	local breakpoints = require("dap.breakpoints").get()
 	breakpoints = core.lua.table.reduce(breakpoints, function(prev_res, cur_item)
-		local buf_name = vim.api.nvim_buf_get_name(cur_item.k)
+		local buf_name = get_buf_name(cur_item.k, root_path)
 		prev_res[buf_name] = cur_item.v
 		return prev_res
 	end, {})
@@ -20,6 +25,7 @@ end
 
 local restore_breakpoints = function(file_path)
 	local core = require("niuiic-core")
+	local root_path = core.file.root_path()
 
 	if not core.file.file_or_dir_exists(file_path) then
 		return
@@ -32,7 +38,7 @@ local restore_breakpoints = function(file_path)
 
 	local breakpoints = vim.fn.json_decode(text)
 	breakpoints = core.lua.list.reduce(vim.api.nvim_list_bufs(), function(prev_res, cur_item)
-		local buf_name = vim.api.nvim_buf_get_name(cur_item)
+		local buf_name = get_buf_name(cur_item, root_path)
 		if breakpoints[buf_name] ~= nil then
 			prev_res[cur_item] = breakpoints[buf_name]
 		end
