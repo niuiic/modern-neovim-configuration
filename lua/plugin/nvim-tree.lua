a58a5405cce127e9426918e9373f0c49022cdf14
niuiic <niuiicde@gmail.com>
2023-06-02 09:43:31 +0800

    update

--- lua/plugin/nvim-tree.lua
+++ lua/plugin/nvim-tree.lua
@@ -61,7 +61,20 @@ local config = function()
 				resize_window = true,
 				window_picker = {
 					picker = function()
-						return require("window-picker").pick_window()
+						return require("window-picker").pick_window({
+							picker_config = {
+								statusline_winbar_picker = {
+									use_winbar = "always",
+								},
+							},
+							show_prompt = false,
+							filter_rules = {
+								bo = {
+									filetype = { "NvimTree", "notify", "noice" },
+									buftype = { "terminal" },
+								},
+							},
+						})
 					end,
 				},
 			},
--- lua/plugin/nvim-window-picker.lua
+++ lua/plugin/nvim-window-picker.lua
@@ -1,28 +1,22 @@
 return {
-	config = function()
-		require("window-picker").setup({
-			hint = "floating-big-letter",
-			highlights = {
-				winbar = {
-					focused = {
-						fg = "#ededed",
-						bg = "#44cc41",
-						bold = true,
-					},
-					unfocused = {
-						fg = "#ededed",
-						bg = "#ff23ff",
-						bold = true,
-					},
-				},
-			},
-		})
-	end,
 	keys = {
 		{
 			"<space>wo",
 			function()
-				local winid = require("window-picker").pick_window()
+				local winid = require("window-picker").pick_window({
+					picker_config = {
+						statusline_winbar_picker = {
+							use_winbar = "always",
+						},
+					},
+					show_prompt = false,
+					filter_rules = {
+						bo = {
+							filetype = { "NvimTree", "notify", "noice" },
+							buftype = { "terminal" },
+						},
+					},
+				})
 				if winid then
 					vim.api.nvim_set_current_win(winid)
 				end
