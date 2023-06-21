b00f7ecc1496c288a039b7e4d8bdbb3479d1d035
niuiic <niuiicde@gmail.com>
2023-06-21 22:54:00 +0800

    update

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -29,9 +29,24 @@ local lspList = {
 }
 
 -- common lsp config
+local au_group = vim.api.nvim_create_augroup("LspInlayHint", {})
+local inlay_hint_supported_filetypes = { "rust", "typescript", "javascript", "lua" }
 local on_attach = function(client, _)
 	-- disable default format feature of lsp
 	client.server_capabilities.documentFormattingProvider = false
+	vim.api.nvim_clear_autocmds({ group = au_group })
+	vim.lsp.buf.inlay_hint(0, true)
+	vim.api.nvim_create_autocmd({ "BufEnter" }, {
+		callback = function(args)
+			local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+			if core.lua.list.includes(inlay_hint_supported_filetypes, function(v)
+				return v == filetype
+			end) then
+				vim.lsp.buf.inlay_hint(0, true)
+			end
+		end,
+		group = au_group,
+	})
 end
 
 -- nvim-cmp support
@@ -70,17 +85,5 @@ vim.diagnostic.config({
 require("lspconfig.ui.windows").default_options = {
 	border = "single",
 }
-
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-	callback = function()
-		vim.lsp.buf.inlay_hint(0, false)
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-	callback = function()
-		vim.lsp.buf.inlay_hint(0, true)
-	end,
-})
 
 vim.cmd("hi LspInlayHint guibg=none guifg=#33FFBD")
