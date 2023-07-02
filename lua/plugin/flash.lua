3e07f28b96fac1bafa63e8d956414e5f5878fe45
niuiic <niuiicde@gmail.com>
2023-07-02 23:31:31 +0800

    update

--- lua/plugin/flash.lua
+++ lua/plugin/flash.lua
@@ -1,12 +1,38 @@
 return {
+	config = function()
+		require("flash").setup({
+			label = {
+				rainbow = {
+					enabled = true,
+				},
+			},
+			jump = {
+				autojump = true,
+				pos = "end",
+			},
+			modes = {
+				char = {
+					enabled = false,
+				},
+			},
+		})
+	end,
 	keys = {
 		{
 			"f",
-			mode = { "n", "v", "x", "o" },
 			function()
-				require("flash").jump()
+				require("flash").jump({ search = { mode = "search" } })
 			end,
 			desc = "flash",
+			mode = { "n", "x", "o" },
+		},
+		{
+			"F",
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "flash treesitter",
+			mode = { "n", "x", "o" },
 		},
 	},
 	event = "VeryLazy",
