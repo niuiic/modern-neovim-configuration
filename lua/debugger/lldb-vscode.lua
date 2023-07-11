87147ec8c8ca607b2489ed4d65564006849a3b94
niuiic <niuiicde@gmail.com>
2023-07-11 23:19:28 +0800

    update

--- lua/debugger/lldb-vscode.lua
+++ lua/debugger/lldb-vscode.lua
@@ -36,10 +36,29 @@ dap.configurations.rust = {
 		program = function()
 			vim.cmd("!cargo build")
 			local root_path = core.file.root_path()
-			local project_name = string.match(root_path, "/([%w_-]+)$")
-			local target = root_path .. "/target/debug/" .. project_name
-			if core.file.file_or_dir_exists(target) then
-				return target
+			local target_dir = root_path .. "/target/debug/"
+			if core.file.file_or_dir_exists(target_dir) then
+				local executable = {}
+				for path, path_type in vim.fs.dir(target_dir) do
+					if path_type == "file" then
+						local perm = vim.fn.getfperm(target_dir .. path)
+						if string.match(perm, "x", 3) then
+							table.insert(executable, path)
+						end
+					end
+				end
+				if #executable == 1 then
+					return target_dir .. executable[1]
+				else
+					local target
+					vim.ui.select(executable, { prompt = "Select executable" }, function(choice)
+						target = choice
+					end)
+					if not target then
+						return target_dir .. executable[1]
+					end
+					return target_dir .. target
+				end
 			else
 				local input_val
 				vim.ui.input(
