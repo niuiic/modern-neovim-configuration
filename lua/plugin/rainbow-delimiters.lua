a88a83c4f3a612d17e809d62dea12df0df216be9
niuiic <niuiicde@gmail.com>
2023-07-12 23:36:19 +0800

    update

--- lua/plugin/rainbow-delimiters.lua
+++ lua/plugin/rainbow-delimiters.lua
@@ -1,13 +1,19 @@
 return {
 	config = function()
+		local colors = { "#0099ff", "#00ff00", "#ff4dc3", "#ffff00", "#ff9933" }
+		for i, color in ipairs(colors) do
+			vim.api.nvim_set_hl(0, "RainbowColor" .. i, { fg = color })
+		end
 		require("rainbow-delimiters.setup")({
 			highlight = {
-				"RainbowDelimiterBlue",
-				"RainbowDelimiterOrange",
-				"RainbowDelimiterGreen",
-				"RainbowDelimiterCyan",
-				"RainbowDelimiterViolet",
-				"RainbowDelimiterYellow",
+				"RainbowColor1",
+				"RainbowColor2",
+				"RainbowColor3",
+				"RainbowColor4",
+				"RainbowColor5",
+			},
+			query = {
+				vue = "",
 			},
 		})
 	end,
