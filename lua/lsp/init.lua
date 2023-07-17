ae9391249a833ac144c7448c7a4e335f01c06eae
niuiic <niuiicde@gmail.com>
2023-07-17 18:46:29 +0800

    update

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -28,29 +28,12 @@ local lspList = {
 	"dartls",
 }
 
--- common lsp config
-local on_attach = function(client, _)
-	-- disable default format feature of lsp
-	client.server_capabilities.documentFormattingProvider = false
-end
-
 -- nvim-cmp support
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- load all lsp config
 for _, value in pairs(lspList) do
 	local config = require("lsp/" .. value)
-
-	-- set on_attach
-	if config.on_attach == nil then
-		config.on_attach = on_attach
-	else
-		local on_attach_from_config = config.on_attach
-		config.on_attach = function(client, bufnr)
-			on_attach(client, bufnr)
-			on_attach_from_config(client, bufnr)
-		end
-	end
 
 	-- set capabilities
 	if config.modify_capabilities == nil then
