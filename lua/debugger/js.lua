560dc577d940fc5514ba8a67fe843219e738baff
niuiic <niuiicde@gmail.com>
2023-07-20 16:54:30 +0800

    update

--- lua/debugger/js.lua
+++ lua/debugger/js.lua
@@ -21,81 +21,83 @@ core.lua.list.each({ "pwa-node", "pwa-chrome" }, function(v)
 	}
 end)
 
-local node = function(run)
-	run({
-		{
-			name = "Launch project",
-			type = "pwa-node",
-			request = "launch",
-			cwd = "${workspaceFolder}",
-			runtimeExecutable = "pnpm",
-			runtimeArgs = {
-				"debug",
-			},
+local node = {
+	{
+		name = "Launch project",
+		type = "pwa-node",
+		request = "launch",
+		cwd = "${workspaceFolder}",
+		runtimeExecutable = "pnpm",
+		runtimeArgs = {
+			"debug",
 		},
-		{
-			name = "Launch cmd",
-			type = "pwa-node",
-			request = "launch",
-			cwd = core.file.root_path(),
-			runtimeExecutable = "pnpm",
-			runtimeArgs = {
-				"debug:cmd",
-			},
+	},
+	{
+		name = "Launch cmd",
+		type = "pwa-node",
+		request = "launch",
+		cwd = core.file.root_path(),
+		runtimeExecutable = "pnpm",
+		runtimeArgs = {
+			"debug:cmd",
 		},
-		{
-			name = "Launch file",
-			type = "pwa-node",
-			request = "launch",
-			program = "${file}",
-			cwd = "${workspaceFolder}",
-		},
-		{
-			name = "Attach",
-			type = "pwa-node",
-			request = "attach",
-			processId = require("dap.utils").pick_process,
-			cwd = "${workspaceFolder}",
-		},
-	})
-end
+	},
+	{
+		name = "Launch file",
+		type = "pwa-node",
+		request = "launch",
+		program = "${file}",
+		cwd = "${workspaceFolder}",
+	},
+	{
+		name = "Attach",
+		type = "pwa-node",
+		request = "attach",
+		processId = require("dap.utils").pick_process,
+		cwd = "${workspaceFolder}",
+	},
+}
 
-local browser = function(run)
-	run({
-		{
-			name = "Launch Chrome",
-			type = "pwa-chrome",
-			request = "launch",
-			runtimeExecutable = "/usr/bin/google-chrome-stable",
-			userDataDir = false,
-			webRoot = "${workspaceFolder}",
-		},
-		{
-			name = "attach chrome",
-			type = "pwa-chrome",
-			request = "attach",
-			port = 9222,
-			webRoot = "${workspaceFolder}",
-		},
-	})
-end
+local browser = {
+	{
+		name = "Launch Chrome",
+		type = "pwa-chrome",
+		request = "launch",
+		runtimeExecutable = "/usr/bin/google-chrome-stable",
+		userDataDir = false,
+		webRoot = "${workspaceFolder}",
+	},
+	{
+		name = "attach chrome",
+		type = "pwa-chrome",
+		request = "attach",
+		port = 9222,
+		webRoot = "${workspaceFolder}",
+	},
+}
 
 dap_utils.setup({
 	typescript = function(run)
 		if core.file.file_contains(core.file.root_path() .. "/package.json", "vue") then
-			browser(run)
+			run(core.lua.list.merge(browser, node))
 		else
-			node(run)
+			run(node)
 		end
 	end,
 	javascript = function(run)
 		if core.file.file_contains(core.file.root_path() .. "/package.json", "vue") then
-			browser(run)
+			run(core.lua.list.merge(browser, node))
 		else
-			node(run)
+			run(node)
 		end
 	end,
-	javascriptreact = browser,
-	typescriptreact = browser,
-	vue = browser,
+	javascriptreact = function(run)
+		run(browser)
+	end,
+	typescriptreact = function(run)
+		run(browser)
+	end,
+	vue = function(run)
+		run(browser)
+	end,
 })
