c01b03f3c681b262a3a138367d35e273c89b2a68
niuiic <niuiicde@gmail.com>
2023-08-10 09:07:14 +0800

    update

--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -44,14 +44,27 @@ local function rename_file()
 end
 
 local function organize_imports()
-	local params = {
-		command = "_typescript.organizeImports",
-		arguments = {
-			vim.api.nvim_buf_get_name(0),
-		},
-	}
-
-	vim.lsp.buf.execute_command(params)
+	local diagnostic_list = vim.diagnostic.get()
+	local diagnostic = core.lua.list.find(diagnostic_list, function(diagnostic)
+		return string.find(diagnostic.message, "Cannot find name") or string.find(diagnostic.message, "is not defined")
+	end)
+	if diagnostic == nil then
+		return
+	end
+	local cur_pos = vim.api.nvim_win_get_cursor(0)
+	vim.api.nvim_win_set_cursor(0, {
+		diagnostic.lnum + 1,
+		diagnostic.col,
+	})
+	vim.lsp.buf.code_action({
+		apply = true,
+		filter = function(action)
+			return action.title == "Add all missing imports"
+				or string.find(action.title, "Update import from")
+				or string.find(action.title, "Add import from")
+		end,
+	})
+	vim.api.nvim_win_set_cursor(0, cur_pos)
 end
 
 local M = {
