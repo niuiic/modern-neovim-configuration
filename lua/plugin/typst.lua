732c2990f8712fca6b5e8fa387e7cbe7b4e5370b
niuiic <niuiicde@gmail.com>
2023-08-18 10:43:06 +0800

    update

--- lua/plugin/typst.lua
+++ lua/plugin/typst.lua
@@ -8,20 +8,27 @@ local preview = function()
 	local core = require("core")
 
 	if vim.bo.filetype ~= "typst" then
+		vim.notify("Not a typst file", vim.log.levels.WARN)
 		return
 	end
 
 	local file_name = vim.api.nvim_buf_get_name(0)
 	if not core.file.file_or_dir_exists(file_name) then
+		vim.notify("Buffer not saved", vim.log.levels.WARN)
 		return
 	end
 
 	local pdf = to_pdf(file_name)
 	if not core.file.file_or_dir_exists(pdf) then
+		vim.notify("No output found", vim.log.levels.WARN)
 		return
 	end
 
-	vim.cmd("!mimeopen " .. pdf)
+	core.job
+		.spawn("mimeopen", {
+			pdf,
+		}, {}, function() end, function() end, function() end)
+		.terminate()
 end
 
 local pdf_list = {}
