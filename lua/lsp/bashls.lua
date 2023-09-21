1ec96ec76624efd9abf8ce626d183f26835ff515
niuiic <niuiicde@gmail.com>
2023-09-21 14:39:37 +0800

    update

--- lua/lsp/bashls.lua
+++ lua/lsp/bashls.lua
@@ -3,6 +3,16 @@ local M = {
 		"sh",
 		"zsh",
 	},
+	commands = {
+		BashlsRename = {
+			function()
+				vim.lsp.buf.rename(nil, {
+					name = "bashls",
+				})
+			end,
+			description = "Rename",
+		},
+	},
 }
 
 return M
