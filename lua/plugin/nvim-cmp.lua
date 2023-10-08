9c0c5bab59841b1790e4985ec4e08aa81644d892
niuiic <niuiicde@gmail.com>
2023-10-08 14:56:23 +0800

    update

--- lua/plugin/nvim-cmp.lua
+++ lua/plugin/nvim-cmp.lua
@@ -91,11 +91,21 @@ local config = function()
 	-- keymap
 	cmp.setup({
 		mapping = {
-			["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-			["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+			["<C-k>"] = cmp.mapping(
+				cmp.mapping.select_prev_item({
+					behavior = cmp.SelectBehavior.Select,
+				}),
+				{ "i", "c" }
+			),
+			["<C-j>"] = cmp.mapping(
+				cmp.mapping.select_next_item({
+					behavior = cmp.SelectBehavior.Select,
+				}),
+				{ "i", "c" }
+			),
 			["<CR>"] = cmp.mapping.confirm({
 				select = true,
-				behavior = cmp.ConfirmBehavior.Replace,
+				behavior = cmp.ConfirmBehavior.Insert,
 			}),
 			["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 			["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
