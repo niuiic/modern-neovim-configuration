d1ccd5a592a0fb4b7700227b87f998e30d6373c7
niuiic <niuiicde@gmail.com>
2023-10-11 17:16:09 +0800

    update

--- lua/plugin/multiple-session.lua
+++ lua/plugin/multiple-session.lua
@@ -3,12 +3,11 @@ return {
 		local core = require("core")
 		local dap_utils = require("dap-utils")
 		require("multiple-session").setup({
-			default_arg_num = 2,
 			on_session_saved = function(session_dir)
 				dap_utils.store_breakpoints(session_dir .. "/breakpoints")
 				dap_utils.store_watches(session_dir .. "/watches")
 				require("quickfix").store_qf(session_dir .. "/quickfix")
-				vim.cmd("wundo" .. session_dir .. "/undo")
+				vim.cmd("wundo " .. session_dir .. "/undo")
 			end,
 			on_session_restored = function(session_dir)
 				if core.file.file_or_dir_exists(session_dir .. "/" .. "breakpoints") then
@@ -21,17 +20,33 @@ return {
 					require("quickfix").restore_qf(session_dir .. "/quickfix")
 				end
 				if core.file.file_or_dir_exists(session_dir .. "/undo") then
-					vim.cmd("rundo" .. session_dir .. "/undo")
+					vim.cmd("rundo " .. session_dir .. "/undo")
 				end
 			end,
 		})
 	end,
 	keys = {
-		{ "<leader>ss", "<cmd>SaveSession<CR>", desc = "save session" },
-		{ "<leader>sr", "<cmd>RestoreSession<CR>", desc = "restore session" },
-		{ "<leader>sa", "<cmd>EnableAutoSaveSession<CR>", desc = "enable auto save session" },
-		{ "<leader>sA", "<cmd>DisableAutoSaveSession<CR>", desc = "disable auto save session" },
-		{ "<leader>sd", "<cmd>DeleteSession<CR>", desc = "delete session" },
+		{
+			"<leader>ss",
+			function()
+				require("multiple-session").save_session()
+			end,
+			desc = "save session",
+		},
+		{
+			"<leader>sr",
+			function()
+				require("multiple-session").restore_session()
+			end,
+			desc = "restore session",
+		},
+		{
+			"<leader>sd",
+			function()
+				require("multiple-session").delete_session()
+			end,
+			desc = "delete session",
+		},
 	},
 	lazy = false,
 }
