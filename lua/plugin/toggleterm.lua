1c7cdb0243079fc68349aa0f86b6ad7404b2ef8d
niuiic <niuiicde@gmail.com>
2023-10-21 09:32:20 +0800

    update

--- lua/plugin/toggleterm.lua
+++ lua/plugin/toggleterm.lua
@@ -1,9 +1,8 @@
 return {
 	config = function()
 		require("toggleterm").setup({
-			size = 15,
 			open_mapping = "<C-z>",
-			direction = "horizontal",
+			direction = "float",
 		})
 	end,
 	keys = {
