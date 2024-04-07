09aaf37dc4e41fbe1d7a46a002a5e87ddd32a90d
niuiic <niuiicde@gmail.com>
2024-04-07 17:31:35 +0800

    update

--- lua/plugin/yankybank.lua
+++ lua/plugin/yankybank.lua
@@ -1,3 +1,7 @@
+vim.cmd(
+	[[autocmd TextYankPost * silent! lua vim.highlight.on_yank {higroup=(vim.fn['hlexists']('HighlightedyankRegion') > 0 and 'HighlightedyankRegion' or 'IncSearch'), timeout=500}]]
+)
+
 return {
 	config = function()
 		require("yankbank").setup({})
