fbcf581a8b4ebb7f3b79530108398ccf1dd23de1
niuiic <niuiicde@gmail.com>
2024-04-23 11:56:18 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -199,6 +199,8 @@ require("lazy").setup({
 	load_plugin_config("Wansmer/treesj", "plugin/treesj"),
 	-- smooth scrolling
 	load_plugin_config("karb94/neoscroll.nvim", "plugin/neoscroll"),
+	-- keep cursor in center
+	load_plugin_config("nkakouros-original/scrollofffraction.nvim", "plugin/scrollofffraction"),
 	-- remove buffer
 	load_plugin_config("echasnovski/mini.bufremove", "plugin/mini-bufremove"),
 	-- format
