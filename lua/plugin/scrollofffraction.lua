b8ad296452ef62332069c54bf003845a1f2b9678
niuiic <niuiicde@gmail.com>
2024-04-23 11:57:01 +0800

    update

--- lua/plugin/scrollofffraction.lua
+++ lua/plugin/scrollofffraction.lua
@@ -3,7 +3,6 @@ return {
 		require("scrollofffraction").setup({
 			scrolloff_fraction = 0.4,
 			scrolloff_absolute_filetypes = { "qf", "markdown" },
-			scrolloff_absolute_value = 5,
 		})
 	end,
 }
