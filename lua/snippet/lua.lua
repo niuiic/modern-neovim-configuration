0a1767ff937f9853c9b052a97266b147ed08f36a
niuiic <niuiicde@gmail.com>
2024-05-18 18:33:42 +0800

    update

--- lua/snippet/lua.lua
+++ lua/snippet/lua.lua
@@ -9,48 +9,6 @@ local M = {
 		i(1),
 		t({ [[))]] }),
 	}),
-	s("tasks", {
-		t({
-			[[local core = require("core")]],
-			"",
-			[[local overseer = require("overseer")]],
-			"",
-			[[local scriptPath = core.file.root_path() .. "/.nvim/task/task.sh"]],
-			"",
-			[[overseer.register_template({]],
-			[[	name = "template",]],
-			[[	builder = function()]],
-			[[		return {]],
-			[[			cmd = { scriptPath },]],
-			[[			args = { "template" },]],
-			[[			components = { "on_exit_set_status", "on_output_quickfix", "on_complete_notify" },]],
-			[[		}]],
-			[[	end,]],
-			[[})]],
-		}),
-	}),
-	s("task", {
-		t({
-			[[overseer.register_template({]],
-			[[	name = "]],
-		}),
-		i(1, "name"),
-		t({
-			[[",]],
-			[[	builder = function()]],
-			[[		return {]],
-			[[			cmd = { scriptPath },]],
-			[[			args = { "]],
-		}),
-		i(2, "args"),
-		t({
-			[[" },]],
-			[[			components = { "on_exit_set_status", "on_output_quickfix", "on_complete_notify" },]],
-			[[		}]],
-			[[	end,]],
-			[[})]],
-		}),
-	}),
 }
 
 return M
