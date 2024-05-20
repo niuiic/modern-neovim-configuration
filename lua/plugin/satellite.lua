0ffd46850047fefe71b3d7f17c4ff7547efd3898
niuiic <niuiicde@gmail.com>
2024-05-20 18:32:56 +0800

    update

---
+++ lua/plugin/satellite.lua
@@ -0,0 +1,5 @@
+return {
+	config = function()
+		require("satellite").setup({})
+	end,
+}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -216,4 +216,6 @@ require("lazy").setup({
 	load_plugin_config("cshuaimin/ssr.nvim", "plugin/ssr"),
 	-- enhance a/i
 	load_plugin_config("echasnovski/mini.ai", "plugin/mini-ai"),
+    -- file map
+	load_plugin_config("lewis6991/satellite.nvim", "plugin/satellite"),
 })
