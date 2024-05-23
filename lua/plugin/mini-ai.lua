0135e9ccb6d128316dbaed954c0d46b05d927fd2
niuiic <niuiicde@gmail.com>
2024-05-23 09:40:20 +0800

    update

--- lua/plugin/mini-ai.lua
+++ lua/plugin/mini-ai.lua
@@ -1,11 +1,6 @@
 return {
 	config = function()
-		local ai = require("mini.ai")
-
 		require("mini.ai").setup({
-			custom_textobjects = {
-				f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
-			},
 			silent = true,
 		})
 	end,
