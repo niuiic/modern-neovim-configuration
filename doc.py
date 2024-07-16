ad48e63131037a710d49a863d411017b81d9c150
niuiic <niuiicde@gmail.com>
2024-07-16 20:12:52 +0800

    update

--- doc.py
+++ doc.py
@@ -1,12 +1,26 @@
 import sys
 import os
 import re
+import json
 from http.client import HTTPSConnection
 from concurrent.futures import ThreadPoolExecutor
 
 if len(sys.argv) < 2:
     print("Usage: python doc.py <GITHUB_TOKEN>")
     sys.exit(1)
+
+
+plugins = {}
+
+
+def collect_plugins(file):
+    with open(file, "r") as f:
+        for line in f:
+            match = re.findall(r'"[^/"\s<>@]+/[^/"\s<>@]+"', line)
+            if match:
+                for plugin in match:
+                    if not plugin[1:-1].startswith("plugin"):
+                        plugins[plugin[1:-1]] = ""
 
 
 def walk_dir(dir, cb):
@@ -18,66 +32,50 @@ def walk_dir(dir, cb):
             cb(path)
 
 
-plugins = set()
-
-
-def collect_plugins(file):
-    with open(file, "r") as f:
-        for line in f:
-            match = re.findall(r'"[^/"\s<>@]+/[^/"\s<>@]+"', line)
-            if match:
-                for plugin in match:
-                    plugins.add(plugin[1:-1])
-    return plugins
-
-
 walk_dir("./lua", collect_plugins)
 
-valid_plugins = []
 
-
-def filter_valid_plugin(plugin):
-    try:
-        if plugin.startswith("plugin/"):
-            return
-        print(f"check {plugin}")
-        conn = HTTPSConnection("api.github.com", timeout=10)
-        conn.request(
-            "GET",
-            f"/repos/{plugin}",
-            headers={
-                "Accept": "application/vnd.github+json",
-                "Authorization": f"Bearer {sys.argv[1]}",
-                "X-GitHub-Api-Version": "2022-11-28",
-                "User-Agent": "App/1.0",
-            },
-        )
-        response = conn.getresponse()
-        conn.close()
-        if response.status >= 400:
-            print(f"{plugin} is invalid")
-            return
-        valid_plugins.append(plugin)
-    except:
-        pass
+def validate_plugin(plugin):
+    print(f"check {plugin}")
+    conn = HTTPSConnection("api.github.com", timeout=10)
+    conn.request(
+        "GET",
+        f"/repos/{plugin}",
+        headers={
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {sys.argv[1]}",
+            "X-GitHub-Api-Version": "2022-11-28",
+            "User-Agent": "App/1.0",
+        },
+    )
+    response = conn.getresponse()
+    if response.status >= 400:
+        del plugins[plugin]
+    else:
+        plugins[plugin] = json.loads(response.read().decode("utf-8"))["description"]
+    conn.close()
 
 
 with ThreadPoolExecutor(max_workers=20) as executor:
-    [executor.submit(filter_valid_plugin, plugin) for plugin in plugins]
+    [executor.submit(validate_plugin, plugin) for plugin in plugins]
 
-valid_plugins = sorted(valid_plugins)
+
+lines = []
+for plugin in plugins:
+    lines.append(
+        f"- [{plugin}](https://github.com/{plugin}) {plugins[plugin]}\n![](https://img.shields.io/github/stars/{plugin}) ![](https://img.shields.io/github/last-commit/{plugin}) ![](https://img.shields.io/github/commit-activity/y/{plugin})\n"
+    )
 
 doc = ""
-
 with open("./README.md", "r") as f:
     for line in f.readlines():
-        doc = doc + line
+        doc += line
         if line.startswith("## Plugins"):
-            doc = doc + "\n"
+            doc += "\n"
             break
-
-for plugin in valid_plugins:
-    doc += f"- [{plugin}](https://github.com/{plugin}) ![](https://img.shields.io/github/stars/{plugin}) ![](https://img.shields.io/github/last-commit/{plugin}) ![](https://img.shields.io/github/commit-activity/y/{plugin})\n"
+lines = sorted(lines)
+for line in lines:
+    doc += line
 
 with open("./README.md", "w") as f:
     f.write(doc)
