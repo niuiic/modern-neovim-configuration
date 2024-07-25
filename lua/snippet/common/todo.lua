310455c61825c4c785d494d1d752c0359fb632ee
niuiic <niuiicde@gmail.com>
2024-07-25 17:08:48 +0800

    update

--- lua/snippet/common/todo.lua
+++ lua/snippet/common/todo.lua
@@ -5,59 +5,14 @@ local i = ls.insert_node
 
 local M = {
 	s("todo", {
-		t({ [[TODO: ]] }),
+		t({ "[ ] (" }),
 		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("verify", {
-		t({ [[VERIFY: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("fake", {
-		t({ [[FAKE: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("mark", {
-		t({ [[MARK: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("hack", {
-		t({ [[HACK: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("fixme", {
-		t({ [[FIXME: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("note", {
-		t({ [[NOTE: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("warn", {
-		t({ [[WARN: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("perf", {
-		t({ [[PERF: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("temp", {
-		t({ [[TEMP: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
-	}),
-	s("question", {
-		t({ [[Q: ]] }),
-		i(1),
-		t({ [[ (niuiic)]] }),
+		t({ ":" }),
+		i(2),
+		t({ "){" }),
+		i(3),
+		t({ "}: " }),
+		i(4),
 	}),
 }
 
