0420b04813d7a5afe2f3a2cd75b7cdc6ec5a3283
niuiic <niuiicde@gmail.com>
2024-09-29 19:38:23 +0800

    update

--- lua/plugin/task.lua
+++ lua/plugin/task.lua
@@ -1,4 +1,21 @@
 return {
+	config = function()
+		local split_win = require("task.output").use_split_win()
+
+		require("task").register({
+			name = "run lua",
+			config = function()
+				return {
+					cmd = "luajit",
+					args = { vim.api.nvim_buf_get_name(0) },
+				}
+			end,
+			on_exit = {
+				require("task.output").notify_done,
+				split_win,
+			},
+		})
+	end,
 	keys = {
 		{
 			"<space>sr",
