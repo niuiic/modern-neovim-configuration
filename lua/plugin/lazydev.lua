581aabc12df476ff2b3af3b4530d2ac055a27f91
niuiic <niuiicde@gmail.com>
2024-10-31 19:50:22 +0800

    update

--- lua/plugin/lazydev.lua
+++ lua/plugin/lazydev.lua
@@ -3,10 +3,9 @@ return {
 		require("lazydev").setup({
 			library = {
 				{ path = "luvit-meta/library", words = { "vim%.uv" } },
-				{ path = "xmake-luals-addon/library", files = { "xmake.lua" } },
 			},
 		})
 	end,
 	ft = "lua",
-	dependencies = { "Bilal2453/luvit-meta", "LelouchHe/xmake-luals-addon" },
+	dependencies = { "Bilal2453/luvit-meta" },
 }
