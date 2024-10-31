22c26d243a12092e060955f87753654343ef544e
niuiic <niuiicde@gmail.com>
2024-10-31 11:16:10 +0800

    update

--- lua/plugin/mini-indentscope.lua
+++ lua/plugin/mini-indentscope.lua
@@ -1,5 +1,12 @@
 return {
 	config = function()
 		require("mini.indentscope").setup()
+		vim.api.nvim_create_autocmd("BufEnter", {
+			callback = function()
+				if vim.bo.filetype == "terminal" then
+					vim.b.miniindentscope_disable = true
+				end
+			end,
+		})
 	end,
 }
