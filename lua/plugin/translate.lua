345b3332c0c9ee3ead08d4d29ffe4d44862c3e18
niuiic <niuiicde@gmail.com>
2024-11-17 02:10:23 +0800

    update

--- lua/plugin/translate.lua
+++ lua/plugin/translate.lua
@@ -1,109 +1,66 @@
-local config = function()
-	require("translate").setup({
-		translate = {
-			{
-				cmd = "TransToZH",
-				command = "curl",
-				args = function(trans_source)
-					return {
-						"-X",
-						"POST",
-						"http://localhost:1188/translate",
-						"-H",
-						"Content-Type: application/json",
-						"-d",
-						vim.fn.json_encode({
-							text = trans_source,
-							source_lang = "EN",
-							target_lang = "ZH",
-						}),
-						"-s",
-					}
-				end,
-				input = "selection",
-				output = { "float_win" },
-				format = function(output)
-					local data = vim.fn.json_decode(output)
-					return data.data
-				end,
-			},
-			{
-				cmd = "TransToEN",
-				command = "curl",
-				args = function(trans_source)
-					return {
-						"-X",
-						"POST",
-						"http://localhost:1188/translate",
-						"-H",
-						"Content-Type: application/json",
-						"-d",
-						vim.fn.json_encode({
-							text = trans_source,
-							target_lang = "EN",
-							source_lang = "ZH",
-						}),
-						"-s",
-					}
-				end,
-				input = "input",
-				output = { "notify", "clipboard" },
-				format = function(output)
-					local data = vim.fn.json_decode(output)
-					return data.data
-				end,
-			},
-			{
-				cmd = "TransToENV",
-				command = "curl",
-				args = function(trans_source)
-					return {
-						"-X",
-						"POST",
-						"http://localhost:1188/translate",
-						"-H",
-						"Content-Type: application/json",
-						"-d",
-						vim.fn.json_encode({
-							text = trans_source,
-							target_lang = "EN",
-							source_lang = "ZH",
-						}),
-						"-s",
-					}
-				end,
-				input = "selection",
-				output = { "notify", "clipboard" },
-				format = function(output)
-					local data = vim.fn.json_decode(output)
-					return data.data
-				end,
-			},
-		},
+local function trans_to_zh()
+	require("translate").translate({
+		get_command = function(input)
+			return {
+				"trans",
+				"-e",
+				"bing",
+				"-b",
+				":zh",
+				input,
+			}
+		end,
+		input = "selection",
+		output = { "open_float" },
+		resolve_result = function(result)
+			if result.code ~= 0 then
+				return nil
+			end
+
+			return string.match(result.stdout, "(.*)\n")
+		end,
+	})
+end
+
+local function trans_to_en()
+	require("translate").translate({
+		get_command = function(input)
+			return {
+				"trans",
+				"-e",
+				"bing",
+				"-b",
+				":en",
+				input,
+			}
+		end,
+		input = "selection",
+		output = { "replace" },
+		resolve_result = function(result)
+			if result.code ~= 0 then
+				return nil
+			end
+
+			return string.match(result.stdout, "(.*)\n")
+		end,
 	})
 end
 
 return {
-	config = config,
 	keys = {
 		{
 			"<C-t>",
-			"<cmd>TransToZH<cr>",
+			function()
+				local selection = require("omega").get_selection()[1]
+				if selection and string.match(selection, "[A-Za-z].+") then
+					trans_to_zh()
+				else
+					trans_to_en()
+				end
+			end,
 			mode = { "n", "x" },
-			desc = "translate en to zh",
-			silent = true,
-		},
-		{
-			"<space>T",
-			"<cmd>TransToEN<cr>",
-			desc = "translate zh to en",
-		},
-		{
-			"<space>T",
-			"<cmd>TransToENV<cr>",
-			mode = { "x" },
-			desc = "translate zh to en",
 			silent = true,
 		},
 	},
+	branch = "dev",
 }
