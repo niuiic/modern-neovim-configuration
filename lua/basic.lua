238ec453da73165e6bc558376331326500405292
niuiic <niuiicde@gmail.com>
2024-11-21 13:45:21 +0800

    update

--- lua/basic.lua
+++ lua/basic.lua
@@ -114,3 +114,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ higroup = higroup, timeout = 500 })
 	end,
 })
+
+-- snippet
+vim.api.nvim_set_hl(0, "SnippetTabstop", {})
