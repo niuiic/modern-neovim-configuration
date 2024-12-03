d7234182c7290a3c4768a03ce52d733e98ef7a8e
niuiic <niuiicde@gmail.com>
2024-12-03 10:42:16 +0800

    update

--- lua/plugin/task/node.lua
+++ lua/plugin/task/node.lua
@@ -1,10 +1,35 @@
 require("task").register_task({
-	name = "build node project",
+	name = "run node project scripts",
 	run = function()
-		require("plugin.task.utils").run_in_term({
-			"cd " .. vim.fs.root(0, "package.json"),
-			"pnpm build",
-		})
+		local scripts = vim.json.decode(
+			vim.fn.join(vim.fn.readfile(vim.fs.root(0, "package.json") .. "/package.json"), "\n")
+		).scripts
+
+		if not scripts or vim.tbl_isempty(scripts) then
+			vim.notify("no scripts found in package.json", vim.log.levels.WARN)
+			return
+		end
+
+		local tasks = vim.tbl_values(scripts)
+
+		if #tasks == 1 then
+			require("plugin.task.utils").run_in_term({
+				"cd " .. vim.fs.root(0, "package.json"),
+				"pnpm " .. tasks[1],
+			})
+			return
+		end
+
+		vim.ui.select(tasks, { prompt = "select script to run" }, function(choice)
+			if not choice then
+				return
+			end
+
+			require("plugin.task.utils").run_in_term({
+				"cd " .. vim.fs.root(0, "package.json"),
+				"pnpm " .. choice,
+			})
+		end)
 	end,
 	is_enabled = function()
 		return vim.fs.root(0, "package.json") ~= nil
