17aec0965390b9a0c53b5255d699e987ca530d28
niuiic <niuiicde@gmail.com>
2025-03-02 19:52:55 +0800

    update

--- lua/plugin/blink-cmp.lua
+++ lua/plugin/blink-cmp.lua
@@ -22,9 +22,6 @@ return {
 				["<A-k>"] = { "snippet_backward", "fallback" },
 				["<C-b>"] = { "scroll_documentation_up", "fallback" },
 				["<C-f>"] = { "scroll_documentation_down", "fallback" },
-				["<C-n>"] = { "fallback" },
-				["<Tab>"] = { "fallback" },
-				["<S-Tab>"] = { "fallback" },
 			},
 			completion = {
 				menu = {
