7179bb5d0da94f19e085498adb4286b676da7696
niuiic <niuiicde@gmail.com>
2025-03-03 14:24:54 +0800

    update

--- lua/plugin/buffers.lua
+++ lua/plugin/buffers.lua
@@ -2,13 +2,12 @@ return {
 	config = function()
 		require("buffers").setup({
 			enable = function(bufnr)
-				local disabled_filetypes =
-					{ "NvimTree", "smear-cursor", "notify", "noice", "", "buffers", "blink-cmp-documentation" }
+				local disabled_filetypes = { "NvimTree", "smear-cursor", "notify", "noice", "", "buffers" }
 
 				local filetype = vim.filetype.match({ filename = vim.api.nvim_buf_get_name(bufnr) })
 					or vim.api.nvim_get_option_value("filetype", { buf = bufnr })
 
-				return filetype and not vim.tbl_contains(disabled_filetypes, filetype)
+				return filetype and not vim.tbl_contains(disabled_filetypes, filetype) and not filetype:find("blink")
 			end,
 			window = { width = 80 },
 		})
