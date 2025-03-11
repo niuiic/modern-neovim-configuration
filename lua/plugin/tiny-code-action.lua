7221f89f24c61ba1bfaff647af610a1a0a795fd6
niuiic <niuiicde@gmail.com>
2025-03-11 22:59:12 +0800

    update

--- lua/plugin/tiny-code-action.lua
+++ lua/plugin/tiny-code-action.lua
@@ -1,16 +1,6 @@
 return {
 	config = function()
-		require("tiny-code-action").setup({
-			telescope_opts = {
-				layout_strategy = "cursor",
-				layout_config = {
-					width = 0.6,
-					height = 0.3,
-					preview_cutoff = 1,
-					prompt_position = "top",
-				},
-			},
-		})
+		require("tiny-code-action").setup()
 	end,
 	keys = {
 		{
