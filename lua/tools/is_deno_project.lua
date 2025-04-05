1c70a19d3a6547eacc0f0db3fdf04540f46517f7
niuiic <niuiicde@gmail.com>
2025-04-05 14:56:40 +0800

    update

--- lua/tools/is_deno_project.lua
+++ lua/tools/is_deno_project.lua
@@ -1,6 +1,4 @@
 return function()
 	local root_dir = vim.fs.root(0, ".git") or vim.fn.getcwd()
-	return not vim.uv.fs_stat(root_dir .. "/package.json")
-		or vim.uv.fs_stat(root_dir .. "/deno.json")
-		or vim.uv.fs_stat(root_dir .. "/deno.lock")
+	return not vim.uv.fs_stat(root_dir .. "/package.json") or vim.uv.fs_stat(root_dir .. "/deno.json")
 end
