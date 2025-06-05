427a42e64c114fe32064093b55423b70bf8518b8
niuiic <niuiicde@gmail.com>
2025-06-05 14:23:37 +0800

    update

--- lua/lsp/denols.lua
+++ lua/lsp/denols.lua
@@ -28,7 +28,7 @@ require("lsp-commands").register_command({
 		vim.lsp.buf.code_action({
 			apply = true,
 			filter = function(action)
-				return action.title == "Add all missing imports"
+				return action.title == "Add all missing imports" or action.title:find("Add import from", 1, true) ~= nil
 			end,
 		})
 		vim.api.nvim_win_set_cursor(0, cur_pos)
