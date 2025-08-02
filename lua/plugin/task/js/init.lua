a88067f097cc6f120d17ec9fe2159931b11ceb39
niuiic <niuiicde@gmail.com>
2025-08-02 10:17:38 +0800

    update

--- lua/plugin/task/js/init.lua
+++ lua/plugin/task/js/init.lua
@@ -80,7 +80,7 @@ require("task").register_task({
 	name = "debug ts file",
 	run = function()
 		require("plugin.task.utils").run_in_term({
-			"deno run --inspect -A " .. vim.api.nvim_buf_get_name(0),
+			"deno run --inspect-brk -A " .. vim.api.nvim_buf_get_name(0),
 		})
 	end,
 	is_enabled = function()
