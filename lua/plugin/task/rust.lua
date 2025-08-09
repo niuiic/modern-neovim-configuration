cd0dfbaf1d6cb7b447f2b0d008fc94ecd81ab6b9
niuiic <niuiicde@gmail.com>
2025-08-09 19:50:30 +0800

    Revert "update"
    This reverts commit aa2e2e94cb4a050748476e8a119493d6c466ef3c.

--- lua/plugin/task/rust.lua
+++ lua/plugin/task/rust.lua
@@ -37,42 +37,14 @@ require("task").register_task({
 	end,
 })
 
-local doc_process = nil
-
 require("task").register_task({
 	name = "open rust project document",
 	run = function()
-		if doc_process then
-			vim.fn.system({ "kill", "-9", doc_process.pid })
-			doc_process = nil
-		end
-
-		vim.system(
-			{ "cargo", "doc" },
-			{ cwd = vim.fs.root(0, "Cargo.toml") },
-			vim.schedule_wrap(function(res)
-				if res.code ~= 0 then
-					vim.notify("failed to build doc", vim.log.levels.ERROR)
-					return
-				end
-
-				local process = vim.system({ "busybox", "httpd", "-f", "-p", "7777" }, {
-					cwd = vim.fs.joinpath(vim.fs.root(0, "Cargo.toml"), "target/doc"),
-				})
-				doc_process = process
-				vim.notify("rust doc opened at http://localhost:7777", vim.log.levels.INFO)
-			end)
-		)
+		vim.system({ "cargo", "doc", "--open" }, {
+			cwd = vim.fs.root(0, "Cargo.toml"),
+		})
 	end,
 	is_enabled = function()
 		return vim.fs.root(0, "Cargo.toml") ~= nil
-	end,
-})
-
-vim.api.nvim_create_autocmd("VimLeave", {
-	callback = function()
-		if doc_process then
-			vim.fn.system({ "kill", "-9", doc_process.pid })
-		end
 	end,
 })
