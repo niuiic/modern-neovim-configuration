ea2b5b0a77d3558fb82295a402d3e906403045fc
niuiic <niuiicde@gmail.com>
2026-04-24 13:10:08 +0800

    update

--- lua/plugin/treesitter-outer.lua
+++
@@ -1,9 +0,0 @@
-return {
-	config = function()
-		require("treesitter-outer").setup({
-			prev_outer_key = "{",
-			next_outer_key = "}",
-		})
-	end,
-	dependencies = "nvim-treesitter/nvim-treesitter",
-}
