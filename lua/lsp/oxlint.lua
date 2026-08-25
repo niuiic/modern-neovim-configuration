ee201d736e27576446d7374fbb49b19bf8543d0e
niuiic <niuiicde@gmail.com>
2026-08-25 14:37:18 +0800

    update

---
+++ lua/lsp/oxlint.lua
@@ -0,0 +1,57 @@
+local function fix_all()
+	local diagnostic_list = vim.diagnostic.get()
+	local diagnostic = vim.iter(diagnostic_list):find(function(diagnostic)
+		return diagnostic.source == "oxlint"
+	end)
+	if diagnostic == nil then
+		return
+	end
+	local cur_pos = vim.api.nvim_win_get_cursor(0)
+	vim.api.nvim_win_set_cursor(0, {
+		diagnostic.lnum + 1,
+		diagnostic.col,
+	})
+	vim.lsp.buf.code_action({
+		apply = true,
+		filter = function(action)
+			return action.title == "Fix all auto-fixable problems"
+		end,
+	})
+	vim.api.nvim_win_set_cursor(0, cur_pos)
+end
+
+local function is_enabled()
+	if not require("tools.lsp_valid")("oxlint") then
+		return false
+	end
+
+	if vim.bo.filetype == "vue" then
+		local node = vim.treesitter.get_node()
+		while node do
+			if
+				string.find(node:sexpr(), "(script_element", 1, true) == 1
+				or string.find(node:sexpr(), "(template_element", 1, true) == 1
+			then
+				return true
+			end
+
+			node = node:parent()
+		end
+
+		return false
+	end
+
+	return true
+end
+
+require("lsp-commands").register_command({
+	name = "oxlint/fix all",
+	run = fix_all,
+	is_enabled = is_enabled,
+})
+
+return {
+	root_dir = function(bufnr, on_dir)
+		on_dir(vim.fs.root(bufnr, ".root") or vim.fs.root(bufnr, ".git") or vim.fn.getcwd())
+	end,
+}
